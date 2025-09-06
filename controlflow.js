68cbee660223dca42e23d5e1214399594ed1e14b
devisrieswar <srieswar14194@gmail.com>
2025-09-06 15:14:54 +0530

    changes make

---
+++ controlflow.js
@@ -0,0 +1,42 @@
+let makes=85;
+if(makes>=90){
+    console.log("A+ grade");
+}else if(makes>=80){
+    console.log("A grade");
+}else if(marks>=70){
+    console.log("B+ grade");
+
+}else{
+    console.log("fail");
+}
+
+let day=5
+switch(day){
+case 1:
+    console.log("monday");
+    break;
+case 2:
+    console.log("tuesday");
+    break;
+case 3:
+    console.log("wednesday");
+    break;
+case 4:
+    console.log("thursday");
+    break;
+case 5:
+    console.log("friday");
+    break;
+case 6:
+    console.log("saturday");
+    break;
+case 7:
+    console.log("sunday");
+    break;
+default:
+    console.log("invalid day");
+    break;
+}
+for(let i=1;i<=5;i++){
+    console.log(i);
+}    
