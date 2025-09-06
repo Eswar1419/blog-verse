cdc58966efa878fcc3de63290624fe6efb49ecc8
devisrieswar <srieswar14194@gmail.com>
2025-09-06 17:12:56 +0530

    changes make

---
+++ asyncAwait.js
@@ -0,0 +1,10 @@
+const simpleData=async ()=>{
+    try{
+    let response=await fetch("https://www.google.com");
+    console.log(response)
+    }catch (err){
+        console.log("error accessing google"+err)
+    }
+
+}
+simpleData()
